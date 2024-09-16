6e3fdbf35aa78f9beb85e8eba74724b27dd696a4
Joshua <Jotdown@town>
2024-09-16 14:02:16 +0530

    prepare base model completed

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from mpoxClassifier import logger
 from mpoxClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from mpoxClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Prepare Base Model Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
